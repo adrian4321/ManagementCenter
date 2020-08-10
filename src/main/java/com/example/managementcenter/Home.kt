92fb751dee5e778f60f27542f1026faa6cc1d0fb
Adrian Hurst <adrian.l.a.hurst@gmail.com>
2020-08-10 11:58:49 -0400

    commit3

--- src/main/java/com/example/managementcenter/Home.kt
+++ src/main/java/com/example/managementcenter/Home.kt
@@ -2,10 +2,39 @@ package com.example.managementcenter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import kotlinx.android.synthetic.main.activity_home.*
 
 class Home : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBar: AppBarConfiguration
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        navController = findNavController(R.id.nav_fragment)
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        appBar = AppBarConfiguration(setOf(R.id.fragmentOne, R.id.fragmentTwo, R.id.fragmentThree), drawerLayout)
+
+        navigationView.setupWithNavController(navController)
+
+
+        setupActionBarWithNavController(navController, appBar)
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_fragment)
+        return  navController.navigateUp(appBar) || super.onSupportNavigateUp()
     }
 }
